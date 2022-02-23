9c491184ca1c0afa02f06347741ddf378c3d7862
Pedro Galvão <pcbgalvao@gmail.com>
2022-02-23 16:19:54 +0000

    Changed from 'fs.readfile' to 'readLine' to support big log files

--- app/log_parser.js
+++ app/log_parser.js
@@ -1,44 +1,76 @@
 const Fs = require('fs');
-// const Regex = require('regex');
+const readLine = require('readline');
 
+
+// prints a help syntax 
 function syntaxHelp(msg) {
     console.log("Syntax: log_parse <string> <logfile>");
 
 }
 
+// Parses data with given string
+function parseData(data, stringToParse) {
+
+    let resultMatch;
+    let regex = new RegExp(`${stringToParse}`, '\g');
+
+    resultMatch = (data.match(regex) || []);
+    return (resultMatch.length);
+
+}
+
+
 function main(argv) {
 
-    // Taking string to parse and log file from command line args
-    var stringToParse = argv[2]
-    var logFile = argv[3]
-    
-    // Syntax Help
+    // Taking string to parse and log file from command line args    
+    var stringToParse = argv[2];
+    var logFile = argv[3];
+
+    // syntax Help
     if (argv[2] === "--help" || argv[2] === "/h") {
 
         syntaxHelp();
         process.exit(0);
     }
 
-    // Test for correct number of args
+    // test for correct number of args
     if (argv.length !== 4) {
         console.log("Warning: Either no string to parse, logfile or both has been provided!");
         syntaxHelp();
         process.exit(1);
     }
 
-    // Tests for the existence of logfile provided
+    // tests for the existence of "logfile" provided
 
     if (!Fs.existsSync(logFile)) {
         console.log(`Error: File "${logFile}" not found!`)
         process.exit(1);
     }
 
-    // Parsing logfile
-    console.log (`Parsing "${logFile}" with string "${stringToParse}"`);
+    // parsing logfile, line by line 
+    //
+    let logFileInterface = {
+        input: Fs.createReadStream(logFile),
+        output: process.stdout,
+        terminal: false
+    }
+    let rl = readLine.createInterface(logFileInterface);
 
+    let matchesFound = 0;
+    rl.on('line', function parseLine(lineText) {
 
-    
+        let matchesResult = parseData(lineText, stringToParse);
+        matchesFound = (matchesFound) + matchesResult;
+
+    });
+
+    rl.on('close', function closeInterface() {
+        // prints the result 
+        console.log(`Matches Found for string "${stringToParse}" : ${matchesFound}`)
+
+    });
 }
+
 
 main(process.argv);
 
